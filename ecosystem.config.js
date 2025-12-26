3d9b2c53e312ade84ccc0f425cf8d9dc31e50c93
刘白菜 <liushuai.baicai@hotmail.com>
2025-12-26 11:26:07 +0800

    格式化ecosystem.config.js文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    apps : [{
-      name   : "mkcert",
-      script : "node index.js",
-      env: {
-          "NODE_ENV": "production",
-          "HOST": "0.0.0.0",
-          "PORT": 80
-      }
-    }]
-  }
+  apps : [{
+    name   : "mkcert",
+    script : "node index.js",
+    env: {
+        "NODE_ENV": "production",
+        "HOST": "0.0.0.0",
+        "PORT": 80
+    }
+  }]
+}
